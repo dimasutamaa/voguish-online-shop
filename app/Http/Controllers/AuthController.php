a95f8a2cce772feba2f6a1a180cee77cf2dbac4e
dimasutamaa <dimasutamapa@gmail.com>
2024-04-09 20:09:58 +0700

    my orders

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\OrderItem;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -80,5 +82,26 @@ class AuthController extends Controller
     public function logout(){
         Auth::logout();
         return redirect()->route('account.login')->with('success', 'You successfully logged out!');
+    }
+
+    public function orders(){
+        $user = Auth::user();
+
+        $orders = Order::where('user_id',$user->id)->orderBy('created_at','DESC')->get();
+
+        $data['orders'] = $orders;
+        return view('front.account.order',$data);
+    }
+
+    public function orderDetail($id){
+        $data = [];
+        $user = Auth::user();
+        $order = Order::where('user_id',$user->id)->where('id',$id)->first();
+        $data['order'] = $order;
+
+        $orderItems = OrderItem::where('order_id',$id)->get();
+        $data['orderItems'] = $orderItems;
+        return view('front.account.order-detail',$data);
+        
     }
 }
