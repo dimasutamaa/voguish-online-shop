77b18c0f8e0f0062669f5a85749ac407948e8185
dimasutamaa <dimasutamapa@gmail.com>
2024-04-16 18:30:22 +0700

    admin - user crud

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -9,8 +9,6 @@
 		<!-- Sidebar user (optional) -->
 		<nav class="mt-2">
 			<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-				<!-- Add icons to the links using the .nav-icon class
-					with font-awesome or any other icon font library -->
 				<li class="nav-item">
 					<a href="dashboard.html" class="nav-link">
 						<i class="nav-icon fas fa-tachometer-alt"></i>
@@ -64,7 +62,7 @@
 					</a>
 				</li>
 				<li class="nav-item">
-					<a href="users.html" class="nav-link">
+					<a href="{{ route('users.index') }}" class="nav-link">
 						<i class="nav-icon  fas fa-users"></i>
 						<p>Users</p>
 					</a>
