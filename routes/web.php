1aae7706539ccc79a25ba9eeb7591ca2c1771291
dimasutamaa <dimasutamapa@gmail.com>
2024-06-11 21:05:48 +0700

    forgot password

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,10 @@ Route::post('/remove-discount',[CartController::class,'removeCoupon'])->name('fr
 Route::post('/add-to-wishlist',[FrontController::class,'addToWishlist'])->name('front.addToWishlist');
 Route::get('/page/{slug}',[FrontController::class,'page'])->name('front.page');
 Route::post('/send-contact-email',[FrontController::class,'sendContactEmail'])->name('front.sendContactEmail');
+Route::get('/forgot-password',[AuthController::class,'forgotPassword'])->name('front.forgotPassword');
+Route::post('/process-forgot-password',[AuthController::class,'processForgotPassword'])->name('front.processForgotPassword');
+Route::get('/reset-password/{token}',[AuthController::class,'resetPassword'])->name('front.resetPassword');
+Route::post('/process-reset-password',[AuthController::class,'processResetPassword'])->name('front.processResetPassword');
 
 Route::group(['prefix' => 'account'], function(){
 
